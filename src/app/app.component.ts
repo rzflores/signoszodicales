cf7f798205fc2ebe70831314f74d7d54d69df5d7
renzo paolo flores chacon <repaflocha@hotmail.com>
2021-05-27 18:35:46 -0500

    signoszodiacales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
+
+import { MatTableDataSource } from '@angular/material/table';
+
+
+export interface signoElement {
+  signo: string; 
+}
+
+
+const Data : signoElement[]=  []
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'SignosZodiacales';
+  title:string = 'SignosZodiacales';
+  value:string = 'Clear me';
+  dataSource  = new MatTableDataSource<signoElement>(Data);
+  isBlank:boolean = false;  
+  displayedColumns: string[] = ['signo'];
+  
+
+  
+
+  borrar(inputvalue:HTMLInputElement){
+    inputvalue.value = '';
+  }
+  buscar = async(inputvalue:HTMLInputElement) => {
+    let dataBody = inputvalue.value == null|| undefined ? JSON.stringify({filtro: "" }) : JSON.stringify({filtro: `${inputvalue.value}`})        
+    
+  
+   await fetch('https://estigia.ctorres.sandbox.ehg.pe/Rest/Caronte/v1.0/getData',{
+       method: 'POST',
+       mode: 'cors',
+       headers: {
+           'Content-Type': 'application/json'               
+         },
+       body: dataBody
+   })
+   
+   .then(response => response.json())
+   .then( (data) => {
+      if(data.signos != null){
+        this.dataSource =  data.signos
+      }else{
+        this.dataSource= new MatTableDataSource<signoElement>(Data);
+        this.isBlank = true;
+      }  
+      
+      
+     }
+    );
+       
+    
+  }
+
+  ngOnInit(){
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button'; 
+import {MatInputModule} from '@angular/material/input'; 
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatTableModule} from '@angular/material/table'; 
+import {MatCardModule} from '@angular/material/card'; 
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,    
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatTableModule,
+    MatCardModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
