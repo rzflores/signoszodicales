dfe9fcc2b7fb1ac064cd60ef60dfce2ce7ce6f43
renzo paolo flores chacon <repaflocha@hotmail.com>
2021-05-28 15:37:17 -0500

    mod

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
